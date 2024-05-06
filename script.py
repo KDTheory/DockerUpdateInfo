7974cada297cb8c53fa3a6ec32963e15bb73dfd2
Kevin Darmon <kdarmondev@gmail.com>
2024-05-06 11:30:12 +0200

    Update script.py
    Update and adding available update or not

--- script.py
+++ script.py
@@ -10,25 +10,38 @@ table_data = []
 for container in containers:
     container_name = container.name
     container_image = container.attrs['Config']['Image']
-
+    current_version = "N/A"
     docker_hub_version = "N/A"
     github_registry_version = "N/A"
 
     if container_image:
-        image_info = client.images.get(container_image)
-        tags = image_info.tags
-        docker_hub_version = tags[0].split(":")[-1] if tags else "N/A"
+        # Récupérer la version actuelle du conteneur local
+        current_version = container.image.tags[0].split(":")[-1] if container.image.tags else "N/A"
 
-        github_registry_url = f"https://api.github.com/repos/{container_image}/tags/"
+        # Récupérer les tags de l'image Docker sur Docker Hub
+        docker_hub_url = f"https://hub.docker.com/v2/repositories/{container_image}/tags"
+        response = requests.get(docker_hub_url)
+        if response.status_code == 200:
+            data = response.json()
+            tags = [tag["name"] for tag in data.get("results", [])]
+            docker_hub_version = tags[0] if tags else "N/A"
+
+        # Récupérer les tags de l'image Docker sur GitHub Registry
+        github_registry_url = f"https://api.github.com/repos/{container_image}/tags"
         response = requests.get(github_registry_url)
         if response.status_code == 200:
-            github_registry_version = response.json()[0]["name"] if response.json() else "N/A"
+            tags = [tag["name"] for tag in response.json()]
+            github_registry_version = tags[0] if tags else "N/A"
+
+    # Comparer les versions pour déterminer s'il y a une mise à jour disponible
+    update_available = docker_hub_version != current_version or github_registry_version != current_version
 
     table_data.append({
         "Container Name": f"{container_name} ({container_image})",
-        "Current Version": container.image.tags[0].split(":")[1] if container.image.tags else "N/A",
+        "Current Version": current_version,
         "Docker Hub Version": docker_hub_version,
-        "GitHub Registry Version": github_registry_version
+        "GitHub Registry Version": github_registry_version,
+        "Update Available": "Yes" if update_available else "No"
     })
 
 print(tabulate(table_data, headers="keys", tablefmt="psql"))
