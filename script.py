c508c6679ff8c986b92bb6bc90182ee23a701810
Kevin Darmon <kdarmondev@gmail.com>
2024-05-06 11:45:27 +0200

    Update script.py

--- script.py
+++ script.py
@@ -10,20 +10,16 @@ table_data = []
 for container in containers:
     container_name = container.name
     container_image = container.attrs['Config']['Image']
-    current_version = "N/A"
     docker_hub_version = "N/A"
     github_registry_version = "N/A"
 
     if container_image:
-        # Get the current version of the local container
-        current_version = container.image.tags[0].split(":")[-1] if container.image.tags else "N/A"
-
         # Get the latest tag of the Docker image on Docker Hub
-        docker_hub_url = f"https://registry.hub.docker.com/v1/repositories/{container_image}/tags"
+        docker_hub_url = f"https://hub.docker.com/v2/repositories/{container_image.split('/')[1]}/tags"
         response = requests.get(docker_hub_url)
         if response.status_code == 200:
             data = response.json()
-            tags = [tag for tag in data]
+            tags = [tag["name"] for tag in data]
             docker_hub_version = max(tags, default="N/A") if tags else "N/A"
 
         # Get the latest tag of the Docker image on GitHub Registry
@@ -35,12 +31,10 @@ for container in containers:
             github_registry_version = max(tags, default="N/A") if tags else "N/A"
 
     # Determine if an update is available
-    update_available = (docker_hub_version != current_version and docker_hub_version != "N/A") or \
-                       (github_registry_version != current_version and github_registry_version != "N/A")
+    update_available = docker_hub_version!= "N/A" or github_registry_version!= "N/A"
 
     table_data.append({
         "Container Name": f"{container_name} ({container_image})",
-        "Current Version": current_version,
         "Docker Hub Version": docker_hub_version,
         "GitHub Registry Version": github_registry_version,
         "Update Available": "Yes" if update_available else "No"
