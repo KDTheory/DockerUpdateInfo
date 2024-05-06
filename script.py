f21321793a8397f4dca51197ae4454241754e3f8
Kevin Darmon <kdarmondev@gmail.com>
2024-05-06 10:37:07 +0200

    Update script.py

--- script.py
+++ script.py
@@ -9,22 +9,27 @@ containers = client.containers.list(all=True)
 table_data = []
 for container in containers:
     container_name = container.name
-    container_image = container.image
-    container_version = container_image.tags[0].split(":")[1]
+    container_image = container.attrs['Config']['Image']
 
-    docker_hub_url = f"https://registry.hub.docker.com/v1/repositories/{container_image}/tags"
-    response = requests.get(docker_hub_url)
-    docker_hub_version = response.json()[0]["name"] if response.status_code == 200 else "N/A"
+    docker_hub_version = "N/A"
+    github_registry_version = "N/A"
 
-    github_registry_url = f"https://api.github.com/repos/{container_name}/tags"
-    response = requests.get(github_registry_url)
-    github_registry_version  = response.json()["files"]["version.txt"]["content"] if response.status_code == 200 else "N/A"
+    if container_image:
+        docker_hub_url = f"https://registry.hub.docker.com/v1/repositories/{container_image}/tags"
+        response = requests.get(docker_hub_url)
+        if response.status_code == 200:
+            docker_hub_version = response.json()[0]["name"]
+
+        github_registry_url = f"https://api.github.com/repos/{container_image}/tags"
+        response = requests.get(github_registry_url)
+        if response.status_code == 200:
+            github_registry_version = response.json()[0]["name"]
 
     table_data.append({
         "Container Name": container_name,
-        "Current Version": container_version,
+        "Current Version": container.image.tags[0].split(":")[1] if container.image.tags else "N/A",
         "Docker Hub Version": docker_hub_version,
-        "GitHub Registry Version": github_registry_version 
+        "GitHub Registry Version": github_registry_version
     })
 
 print(tabulate(table_data, headers="keys", tablefmt="psql"))
